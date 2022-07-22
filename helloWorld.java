bcd1a36a9b3f5ae7a0b3f2427a1cad64eab5d863
Ganesh Rai <ganesh@rai.com>
2022-07-22 10:34:03 +0530

    ⚒️ testing

--- helloWorld.java
+++ helloWorld.java
@@ -7,3 +7,12 @@ public static helloWorld {
         String.out.println("Hello World!");
     }
 }
+
+public static helloWorld {
+    /* this is where the program starts
+     * also this is a multi line comment
+     */
+    public static void main(string[] args) {
+        String.out.println("Hello World!");
+    }
+}
